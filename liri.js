ca3e65fd16dddefeeb644a21993307d1ce967bc6
craigcompton <39470012+craigcompton@users.noreply.github.com>
2018-10-19 14:26:52 -0700

    Add files via upload
    Aaron just slacked out the solution, so I figured that I would commit what I had been working on at this point.  I had been having problems with wrapping my head around the syntax of this assignment.

--- liri.js
+++ liri.js
@@ -9,66 +9,77 @@ var queryText = process.argv[3];
 var keys = require("./keys.js");
 
 // moment().format();
-
-
-
-// queryText = "i write sins not tragedies";
-
-// concert-this
-// spotify-this-song
-// movie-this
-// do-what-it-says
-
-switch (task) {
-	case "concert-this":
-	concertFunction(queryText);
-	break;
-
-	case "spotify-this-song":
-	spotifyFunction(queryText);
-	break;
-
-	case "movie-this":
-	movieFunction(queryText);
-	break;
-
-	case "do-what-it-says":
-	whatFunction();
-	break;
-};
+// console.log("What type of search do you want to do?");
+inquirer.prompt([
+	{
+		name: "searchType",
+		type: "list",
+		message: "What type of search do you want to do?",
+		choices: ["Search for a concert date on Bands in Town?", "Search for a song on Spotify?", "Search for information about a movie on OMDB?", "Or, do you want to 'do what it says?'"]
+	}
+]).then(function (inquirerResponse) {
+	var input = inquirerResponse.searchType
+	if (input === "Search for a concert date on Bands in Town?" || input === "Search for a song on Spotify" || input === "Search for information about a movie on OMDB?" || input === "Or, do you want to 'do what it says?'") {
+		inquirer.prompt([
+			{
+				type: "input",
+				message: "What would you like to search for?",
+				name: "search"
+			},
+		]).then(function (data) {
+			if (input === "Search for a concert date on Bands in Town?") {
+				var concertSearch = new concertFunction();
+				concertSearch.findConcert(data.search);
+			}
+			else if (input === "Search for a song on Spotify") {
+				// var newActor = new TV();
+				// newActor.findActor(data.search);
+			}
+			else if (input === "Search for information about a movie on OMDB?") {
+				// var newActor = new TV();
+				// newActor.findActor(data.search);
+			}
+			else (input === "Or, do you want to 'do what it says?'"); {
+				// var newActor = new TV();
+				// newActor.findActor(data.search);
+			}
+		})
+	}
+});
 
 // Function for Bands In Town - concert API
-var concertFunction = function (queryText) {
-    var queryURL = "https://rest.bandsintown.com/artists/" + queryText + "/events?app_id=codingbootcamp";
-
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    }).then(function (response) {
-        console.log('\n' + queryText + " will be playing at the " + response.venue + " in " + response.venue.city + " at " + moment(response.datetime).format("dddd, MMMM Do YYYY, h:mm a"));
-    });
+var concertFunction = function () {
+	this.findConcert = function (concert) {
+		var queryURL = "https://rest.bandsintown.com/artists/" + concert + "/events?app_id=codingbootcamp";
+		request(queryURL, function (error, response, body) {
+			var data = JSON.parse(body);
+			console.log('\n' + queryText + " will be playing at the " + response.venue + " in " + response.venue.city + " at " + moment(response.datetime).format("dddd, MMMM Do YYYY, h:mm a"));
+		}
+		)
+	}
 };
 
+
 // Function for Node-Spotify-API
-var spotifyFunction = function(queryText) {
+var spotifyFunction = function (queryText) {
 
 	var spotify = new Spotify(keys.spotifyKeys);
-		if (!queryText){
-        	queryText = 'The Sign';
-    	}
-		spotify.search({ type: 'track', query: queryText }, function(err, data) {
-			if (err){
-	            console.log('Error occurred: ' + err);
-	            return;
-	        }
+	if (!queryText) {
+		queryText = 'The Sign';
+	}
+	spotify.search({ type: 'track', query: queryText }, function (err, data) {
+		if (err) {
+			console.log('Error: ' + err);
+			return;
+		}
 
-	        var songInfo = data.tracks.items;
-	        console.log("\nArtist: " + songInfo[0].artists[0].name);
-	        console.log("\nSong's Name: " + songInfo[0].name);
-	        console.log("\nPreview Link fromSpotify: " + songInfo[0].preview_url);
-	        console.log("\nAlbum that the song is from: " + songInfo[0].album.name);
-	});
-}
+		var songInfo = data.tracks.items;
+		console.log("\nArtist: " + songInfo[0].artists[0].name);
+		console.log("\nSong's Name: " + songInfo[0].name);
+		console.log("\nPreview Link fromSpotify: " + songInfo[0].preview_url);
+		console.log("\nAlbum that the song is from: " + songInfo[0].album.name);
+	})
+};
 
 // 
 var movieFunction = function (queryText) {
@@ -79,32 +90,3 @@ var whatFunction = function (queryText) {
 
 };
 
-// ----------------------------------
-// node liri.js movie-this '<movie name here>'
-
-// This will output the following information to your terminal/bash window:
-
-//    * Title of the movie.
-//    * Year the movie came out.
-//    * IMDB Rating of the movie.
-//    * Rotten Tomatoes Rating of the movie.
-//    * Country where the movie was produced.
-//    * Language of the movie.
-//    * Plot of the movie.
-//    * Actors in the movie.
-
-// If the user doesn't type a movie in, the program will output data for the movie 'Mr. Nobody.'
-
-// If you haven't watched "Mr. Nobody," then you should: http://www.imdb.com/title/tt0485947/
-
-// It's on Netflix!
-
-// You'll use the request package to retrieve data from the OMDB API. Like all of the in-class activities, the OMDB API requires an API key. You may use trilogy.
-
-// ----------------------------------------------
-// node liri.js do-what-it-says
-
-// Using the fs Node package, LIRI will take the text inside of random.txt and then use it to call one of LIRI's commands.
-
-// It should run spotify-this-song for "I Want it That Way," as follows the text in random.txt.
-// Edit the text in random.txt to test out the feature for movie-this and my-tweets
